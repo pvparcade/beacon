d12dfed45079d64c0b1752178c578ff8cb3a12bd
Noel <96398669+noelvnx@users.noreply.github.com>
2022-06-09 20:51:39 -0400

    feat: add getAllServers method to api

--- src/main/java/dev/pvparcade/beacon/api/Beacon.java
+++ src/main/java/dev/pvparcade/beacon/api/Beacon.java
@@ -8,10 +8,30 @@ import java.util.List;
 
 public class Beacon {
 
+    /**
+     * Returns a list of every server that is currently running.
+     *
+     * @return a list of servers
+     */
+    public static List<Server> getAllServers() {
+        return BeaconPlugin.getPlugin().getServerManager().getServerMap().values().stream().toList();
+    }
+
+    /**
+     * Returns a specific server by it's id.
+     *
+     * @return a server
+     */
     public static Server getServerFromId(String id) {
         return BeaconPlugin.getPlugin().getServerManager().getServerMap().get(id);
     }
 
+    /**
+     * Returns a list of servers that are in a specific category.
+     *
+     * @param category the category of servers you want to get
+     * @return a list of servers
+     */
     public static List<Server> getServersFromCategory(String category) {
         List<Server> servers = new ArrayList<>();
 
@@ -23,6 +43,12 @@ public class Beacon {
         return servers;
     }
 
+    /**
+     * Returns the least populated server by player count in a specific category.
+     *
+     * @param category the category of servers you want to get
+     * @return a server
+     */
     public static Server getLeastPopulatedServerFromCategory(String category) {
         List<Server> servers = getServersFromCategory(category);
 
